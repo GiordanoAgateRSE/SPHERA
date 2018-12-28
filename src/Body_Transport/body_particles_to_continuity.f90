654776b39633fd7b39bdbb925c56140c196d13e5
AndreaAmicarelliRSE <Andrea.Amicarelli@rse-web.it>
2018-12-28 11:47:47 +0100

    Minor modifications.    Body_Transport. The search for the fluid-body interaction normals is only       carried out in case of free-slip conditions.

--- src/Body_Transport/body_particles_to_continuity.f90
+++ src/Body_Transport/body_particles_to_continuity.f90
@@ -67,73 +67,76 @@ do npi=1,n_body_part
       npartint = (npi - 1) * NMAXPARTJ + j
       npj = PartIntorno_bp_f(npartint)
 ! Continuity equation
-! Normal for u_SA
-      dis_min = 1.d9
-      aux_nor(:) = 0.d0
+! Normal for fluid-body interactions (only for free-slip conditions): start.
+      if (FSI_free_slip_conditions.eqv..true.) then
+         dis_min = 1.d9
+         aux_nor(:) = 0.d0
 ! Here the array PartIntorno_bp_bp cannot be used as it only refers to 
 ! neighbouring body particles of other bodies
 ! Here the array PartIntorno_bp_f cannot be used as usually as it refers to the 
 ! fluid neighbours of a body particles, not to the solid neighbours of a fluid 
 ! particle.
-      do k=1,n_body_part
-         aux_vec(:) = bp_arr(k)%pos(:) - pg(npj)%coord(:) 
-         dis_fb_sb = dsqrt(dot_product(aux_vec,aux_vec))
-         if (dis_fb_sb<=2.d0*Domain%h) then
+         do k=1,n_body_part
+            aux_vec(:) = bp_arr(k)%pos(:) - pg(npj)%coord(:) 
+            dis_fb_sb = dsqrt(dot_product(aux_vec,aux_vec))
+            if (dis_fb_sb<=2.d0*Domain%h) then
 ! The candidate solid particle "k" (or "sb") has a non-null normal and lies 
 ! within the kernel support of the fluid particle "npj" (or "fb")
-            if (bp_arr(k)%body==bp_arr(npi)%body) then
-               aux=dot_product(rag_bp_f(:,npartint),bp_arr(k)%normal)
-               if (aux>1.d-12) then
+               if (bp_arr(k)%body==bp_arr(npi)%body) then
+                  aux=dot_product(rag_bp_f(:,npartint),bp_arr(k)%normal)
+                  if (aux>1.d-12) then
 ! The fluid particle "npj" and the solid particle "k" can "see" each other
-                  if (npi==k) then
-                     aux_nor(:) = bp_arr(k)%normal(:)
-                     exit
-                     else
-                        aux_vec(:) = bp_arr(npi)%pos(:) - bp_arr(k)%pos(:)
-                        dis_s0_sb = dsqrt(dot_product(aux_vec,aux_vec))
-                        aux_vec(:) = bp_arr(npi)%pos(:) - pg(npj)%coord(:) 
-                        dis_fb_s0 = dsqrt(dot_product(aux_vec,aux_vec))
-                        if ((dis_s0_sb<=dis_fb_s0).and.(dis_fb_sb<=dis_fb_s0)) &
-                           then
+                     if (npi==k) then
+                        aux_nor(:) = bp_arr(k)%normal(:)
+                        exit
+                        else
+                           aux_vec(:) = bp_arr(npi)%pos(:) - bp_arr(k)%pos(:)
+                           dis_s0_sb = dsqrt(dot_product(aux_vec,aux_vec))
+                           aux_vec(:) = bp_arr(npi)%pos(:) - pg(npj)%coord(:) 
+                           dis_fb_s0 = dsqrt(dot_product(aux_vec,aux_vec))
+                           if ((dis_s0_sb<=dis_fb_s0).and.                     &
+                              (dis_fb_sb<=dis_fb_s0)) then
 ! The candidate solid particle "k" (or "sb") lies "between" the fluid particle 
 ! "npj" (or "fb") and the solid particle "npi" (or "s0"). The candidate lies 
 ! within the intersection of two equal spheres of radius dis_fb_s0 and centres 
 ! x_fb and x_s0.
-                           call distance_point_line_3D                         &
-                              (bp_arr(k)%pos,bp_arr(npi)%pos,pg(npj)%coord,dis)
-                           dis_min=min(dis_min,dis)
-                           if (dis==dis_min) aux_nor(:) = bp_arr(k)%normal(:)
-                        endif
+                              call distance_point_line_3D                      &
+                               (bp_arr(k)%pos,bp_arr(npi)%pos,pg(npj)%coord,dis)
+                              dis_min=min(dis_min,dis)
+                              if (dis==dis_min) aux_nor(:) = bp_arr(k)%normal(:)
+                           endif
+                     endif
                   endif
                endif
             endif
-         endif
-      enddo
-      mod_normal = dsqrt(dot_product(aux_nor,aux_nor))
-      if (mod_normal<9.99d-1) then
+         enddo
+         mod_normal = dsqrt(dot_product(aux_nor,aux_nor))
+         if (mod_normal<9.99d-1) then
 ! In case the normal is not yet defined, there is a fluid-solid mass 
 ! penetration. The normal vector is taken from the neighbouring surface body 
 ! particle, which is the closest to the neighbouring fluid particle.
-         write(ulog,'(3a)') 'The search for the fluid-body interaction ',      &
-            'normals detects a fluid-solid mass penetration (in case of ',     &
-            'no-slip conditions there is no detection).'
-         write(ulog,'(a,i10,a,i10,a,i10,a,i10,a)')                             &
-            'Computational body particle: ',npi,                               &
-            ' . Neighbouring fluid particle: ',npj,                            &
-            ' . Neighbouring body particle: ',k,                               &
-            ' . Current time step: ',on_going_time_step,' .'
+            write(ulog,'(3a)') 'The search for the fluid-body interaction ',   &
+               'normals detects a fluid-solid mass penetration (in case of ',  &
+               'no-slip conditions there is no detection).'
+            write(ulog,'(a,i10,a,i10,a,i10,a,i10,a)')                          &
+               'Computational body particle: ',npi,                            &
+               ' . Neighbouring fluid particle: ',npj,                         &
+               ' . Neighbouring body particle: ',k,                            &
+               ' . Current time step: ',on_going_time_step,' .'
 ! Here the array PartIntorno_bp_bp cannot be used as it only refers to 
 ! neighbouring body particles of other bodies
 ! Here the array PartIntorno_bp_f cannot be used as it refers to the 
 ! fluid neighbours of a body particles, not to the solid neighbours of a fluid 
 ! particle.
-         do k=1,n_surf_body_part
-            aux_vec(:) = bp_arr(surf_body_part(k))%pos(:) - pg(npj)%coord(:) 
-            dis = dsqrt(dot_product(aux_vec,aux_vec))
-            dis_min = min(dis_min,dis)
-            if (dis==dis_min) aux_nor(:) = bp_arr(k)%normal(:)
-         enddo
+            do k=1,n_surf_body_part
+               aux_vec(:) = bp_arr(surf_body_part(k))%pos(:) - pg(npj)%coord(:) 
+               dis = dsqrt(dot_product(aux_vec,aux_vec))
+               dis_min = min(dis_min,dis)
+               if (dis==dis_min) aux_nor(:) = bp_arr(k)%normal(:)
+            enddo
+         endif
       endif
+! Normal for fluid-body interactions (only for free-slip conditions): end.
 ! Relative velocity for the continuity equation     
       aux_vec(:) = bp_arr(npi)%vel(:) - pg(npj)%vel(:)
       if (FSI_free_slip_conditions.eqv..true.) then
